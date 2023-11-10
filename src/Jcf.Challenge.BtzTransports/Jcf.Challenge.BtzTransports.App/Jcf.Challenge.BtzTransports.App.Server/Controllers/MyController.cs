7e4a0c77278d523d455cb864eb05e742558b5377
Joao C Franca <joaocariasdefranca@gmail.com>
2023-11-09 22:25:53 -0300

    create myController - my default controller

---
+++ src/Jcf.Challenge.BtzTransports/Jcf.Challenge.BtzTransports.App/Jcf.Challenge.BtzTransports.App.Server/Controllers/MyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Jcf.Challenge.BtzTransports.App.Server.Controllers
+{
+    [Authorize]
+    public class MyController : ControllerBase
+    {
+        protected Guid? GetUserIdFromToken()
+        {
+            try
+            {
+                var cid = (HttpContext.User.Identity as ClaimsIdentity)?.Claims.FirstOrDefault(x => x.Type.Equals("USER_ID"));
+                if (cid == null || !Guid.TryParse(cid.Value, out var id))
+                    return null;
+                return id;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("1" + ex.ToString());
+            }
+
+            return null;
+        }
+    }
+}
